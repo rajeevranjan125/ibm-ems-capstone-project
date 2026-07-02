6efc5f3c7283fb56e1e334a021e0b51c2f855244
Rajeev Prasad <rajeev.ranjan.prasad1@ibm.com>
2026-07-02 15:51:14 +0530

    added payroll service module

---
+++ payroll-service/src/main/java/com/ibm.payroll/PayrollServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollServiceApplication.class, args);
+    }
+}
